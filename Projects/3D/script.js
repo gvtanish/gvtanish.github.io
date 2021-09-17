0fd28cab5fb757d41adfa45d811278721ba715b8
G V TANISH VETTRIVEL <76099592+gvtanish@users.noreply.github.com>
2021-09-17 14:59:20 +0530

    Added 3D Robot
    Added 3D Robot

---
+++ Projects/3D/script.js
@@ -0,0 +1,11 @@
+var modelViewer = document.getElementById('model');
+
+function stop() {
+  modelViewer.animationName = 'none';
+}
+function run() {
+  modelViewer.animationName = 'Running';
+}
+function wave() {
+  modelViewer.animationName = 'Wave';
+}
